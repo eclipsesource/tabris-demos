438c07043dad0857c123b832bf6e2c109d589c37
Johannes Eickhold <jeick@eclipsesource.com>
2012-06-22 10:55:21 +0200

    Disabled entry point to guide which was accidentally left in config.

--- com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/Configuration.java
+++ com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/Configuration.java
@@ -17,7 +17,6 @@ import com.eclipsesource.tabris.demos.entrypoints.GeolocationDemo;
 import com.eclipsesource.tabris.demos.entrypoints.InputControlsDemo;
 import com.eclipsesource.tabris.demos.entrypoints.SimpleTreeDemo;
 import com.eclipsesource.tabris.demos.entrypoints.VirtualTreeDemo;
-import com.eclipsesource.tabris.demos.entrypoints.VisualGuideDemo;
 
 public class Configuration implements ApplicationConfiguration {
 
@@ -29,6 +28,5 @@ public class Configuration implements ApplicationConfiguration {
     application.addEntryPoint( "/gallery", GalleryDemo.class, null );
     application.addEntryPoint( "/location", GeolocationDemo.class, null );
     application.addEntryPoint( "/draw", DrawDemo.class, null );
-    application.addEntryPoint( "/guide", VisualGuideDemo.class, null );
   }
 }
