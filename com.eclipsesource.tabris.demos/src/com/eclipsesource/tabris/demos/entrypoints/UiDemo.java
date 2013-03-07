3704478e8fce36a7373fabfe5aaf297abc4c6c71
hstaudacher <hstaudacher@eclipsesource.com>
2013-03-07 15:54:46 +0100

    Adopts latest API changes form TabrisUI

--- com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/UiDemo.java
+++ com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/UiDemo.java
@@ -14,11 +14,11 @@ import org.eclipse.rap.rwt.application.EntryPoint;
 import org.eclipse.rap.rwt.application.EntryPointFactory;
 
 import com.eclipsesource.tabris.demos.ui.BookStoreConfiguration;
-import com.eclipsesource.tabris.ui.TabrisUI;
+import com.eclipsesource.tabris.ui.TabrisUIEntryPoint;
 
 public class UiDemo implements EntryPointFactory {
 
   public EntryPoint create() {
-    return new TabrisUI( new BookStoreConfiguration() );
+    return new TabrisUIEntryPoint( new BookStoreConfiguration() );
   }
 }
