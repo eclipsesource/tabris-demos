501e6ce145affb92a54c774d5e048cf8f5ee5124
hstaudacher <hstaudacher@eclipsesource.com>
2013-12-04 17:08:23 +0100

    Beautifies Button Demo.

--- com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/ButtonControlsDemo.java
+++ com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/ButtonControlsDemo.java
@@ -37,12 +37,12 @@ public class ButtonControlsDemo implements EntryPoint {
   private Button buttonPrevious;
   private Label labelQuestion;
 
+  @Override
   public int createUI() {
     Display display = new Display();
     Shell shell = new Shell( display, SWT.NO_TRIM );
     shell.setMaximized( true );
     GridLayoutFactory.fillDefaults().applyTo( shell );
-    shell.setBackground( display.getSystemColor( SWT.COLOR_BLACK ) );
     createToolbar( shell );
     createContent( shell );
     createNavigation( shell );
@@ -54,11 +54,11 @@ public class ButtonControlsDemo implements EntryPoint {
   private void createToolbar( Composite parent ) {
     ToolBar toolBar = new ToolBar( parent, SWT.NONE );
     GridDataFactory.fillDefaults().grab( true, false ).align( SWT.FILL, SWT.TOP ).applyTo( toolBar );
-    
+
     ToolItem titleItem = new ToolItem( toolBar, SWT.NONE );
     Widgets.onToolItem( titleItem ).useAsTitle();
     titleItem.setText( "You don't know that?" );
-    
+
     ToolItem resetItem = new ToolItem( toolBar, SWT.PUSH );
     resetItem.setText( "Restart" );
     resetItem.addSelectionListener( new SelectionAdapter() {
@@ -69,7 +69,7 @@ public class ButtonControlsDemo implements EntryPoint {
       }
     } );
   }
-  
+
   private void createContent( Composite parent ) {
     Composite content = new Composite( parent, SWT.NONE );
     GridDataFactory.fillDefaults().grab( true, true ).align( SWT.FILL, SWT.FILL ).applyTo( content );
@@ -84,16 +84,16 @@ public class ButtonControlsDemo implements EntryPoint {
     FontDescriptor labelFontDescriptor = FontDescriptor.createFrom( labelQuestion.getFont() );
     FontDescriptor bigFontDescriptor = labelFontDescriptor.setStyle( SWT.BOLD ).increaseHeight( 7 );
     labelQuestion.setFont( bigFontDescriptor.createFont( parent.getDisplay() ) );
-    labelQuestion.setForeground( new Color( parent.getDisplay(), 219, 219, 219 ) );
+    labelQuestion.setForeground( parent.getDisplay().getSystemColor( SWT.COLOR_BLACK ) );
   }
-  
+
   private void createAnswers( Composite parent ) {
     Composite result = new Composite( parent, SWT.NONE );
     GridDataFactory.fillDefaults().grab( true, true ).align( SWT.FILL, SWT.FILL ).applyTo( result );
     GridLayoutFactory.fillDefaults().applyTo( result );
     buttonA = createAnswerButton( result, new RGB(43, 232, 105) );
     buttonB = createAnswerButton( result, new RGB(182, 107, 153) );
-    buttonC = createAnswerButton( result, new RGB(225, 134, 87) );    
+    buttonC = createAnswerButton( result, new RGB(225, 134, 87) );
   }
 
   private Button createAnswerButton( Composite result, RGB color ) {
@@ -114,13 +114,13 @@ public class ButtonControlsDemo implements EntryPoint {
     Composite navigation = new Composite( parent, SWT.NONE );
     GridDataFactory.fillDefaults().grab( true, false ).align( SWT.FILL, SWT.FILL ).applyTo( navigation );
     GridLayoutFactory.fillDefaults().numColumns( 2 ).margins( 8, 8 ).equalWidth( true ).applyTo( navigation );
-    
+
     createNavigationPrevious( navigation );
     createNavigationNext( navigation );
   }
 
   public void createNavigationNext( Composite parent ) {
-    buttonNext = new Button( parent, SWT.PUSH );
+    buttonNext = new Button( parent, SWT.PUSH | SWT.BORDER );
     buttonNext.setText( "Next Question" );
     GridDataFactory.fillDefaults().grab( true, true ).align( SWT.END, SWT.END ).applyTo( buttonNext);
     buttonNext.addSelectionListener( new SelectionAdapter() {
@@ -146,14 +146,13 @@ public class ButtonControlsDemo implements EntryPoint {
   /**
    * Update UI state
    */
-  
   protected void answerSelected( String answer ) {
     String currentQuestion = questions.getCurrentQuestion();
     boolean correct = questions.isCorrect( currentQuestion, answer );
     if( correct ) {
-      buttonNext.setBackground( buttonNext.getDisplay().getSystemColor( SWT.COLOR_DARK_GREEN ) );
+      buttonNext.setForeground( buttonNext.getDisplay().getSystemColor( SWT.COLOR_DARK_GREEN ) );
     } else {
-      buttonNext.setBackground( buttonNext.getDisplay().getSystemColor( SWT.COLOR_DARK_RED ) );
+      buttonNext.setForeground( buttonNext.getDisplay().getSystemColor( SWT.COLOR_DARK_RED ) );
     }
     buttonNext.setEnabled( correct && questions.hasNextQuestion() );
   }
@@ -169,19 +168,18 @@ public class ButtonControlsDemo implements EntryPoint {
     buttonA.getParent().layout();
     buttonPrevious.setEnabled( questions.hasPreviousQuestion() );
     buttonNext.setEnabled( false );
-    buttonNext.setBackground( buttonNext.getDisplay().getSystemColor( SWT.COLOR_DARK_GRAY ) );
   }
 
   /**
    * Navigate the Questions
    */
-  
+
   private void previous() {
     questions.getPreviousQuestion();
     resetButtons();
     updateUI();
   }
-  
+
   protected void next() {
     questions.getNextQuestion();
     resetButtons();
@@ -199,5 +197,5 @@ public class ButtonControlsDemo implements EntryPoint {
     updateUI();
     resetButtons();
   }
-  
+
 }
