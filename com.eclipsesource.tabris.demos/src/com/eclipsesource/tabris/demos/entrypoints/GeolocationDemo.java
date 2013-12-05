b6902cc1bd37b1c205f8d2ae87dcf012c390e624
hstaudacher <hstaudacher@eclipsesource.com>
2013-12-05 16:50:07 +0100

    Reduces Geolocation demo

--- com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/GeolocationDemo.java
+++ com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/GeolocationDemo.java
@@ -12,31 +12,20 @@ package com.eclipsesource.tabris.demos.entrypoints;
 
 import static com.eclipsesource.tabris.widgets.enhancement.Widgets.onToolItem;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.text.DecimalFormat;
-
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.rap.json.JsonArray;
-import org.eclipse.rap.json.JsonObject;
 import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.application.EntryPoint;
+import org.eclipse.rap.rwt.widgets.DialogUtil;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
@@ -47,36 +36,34 @@ import com.eclipsesource.tabris.geolocation.GeolocationListener;
 import com.eclipsesource.tabris.geolocation.GeolocationOptions;
 import com.eclipsesource.tabris.geolocation.Position;
 import com.eclipsesource.tabris.geolocation.PositionError;
+import com.eclipsesource.tabris.interaction.AppLauncher;
+import com.eclipsesource.tabris.interaction.MapsOptions;
 
 
 public class GeolocationDemo implements EntryPoint {
 
   private static final double SPRINGFIELD_LAT = 44.050953;
   private static final double SPRINGFIELD_LON = -123.016663;
+
   private Shell shell;
   private Geolocation geolocation;
-  private Browser browser;
-  private double lastLat;
-  private double lastLon;
-  private String lastLabel;
 
+  @Override
   public int createUI() {
-    lastLat = 49.00612809217996;
-    lastLon = 8.400545271791982;
     createShell();
     createTitle();
-    createBrowser();
     geolocation = RWT.getClient().getService( Geolocation.class );
-    Composite container = new Composite( shell, SWT.NONE );
-    container.setLayoutData( GridDataFactory.fillDefaults()
-      .align( SWT.FILL, SWT.FILL )
-      .grab( true, false )
-      .create() );
-    container.setLayout( GridLayoutFactory.fillDefaults().margins( 0, 5 ).create() );
-    createGetLocationButton( container );
-    createSpringfieldButton( container );
+    createContent();
     shell.open();
     return 0;
+  }
+
+  private void createContent() {
+    Composite container = new Composite( shell, SWT.NONE );
+    container.setLayoutData( GridDataFactory.fillDefaults() .align( SWT.FILL, SWT.FILL ) .grab( true, false ) .create() );
+    container.setLayout( GridLayoutFactory.fillDefaults().margins( 5, 5 ).create() );
+    createGetLocationButton( container );
+    createSpringfieldButton( container );
   }
 
   private void createShell() {
@@ -84,14 +71,6 @@ public class GeolocationDemo implements EntryPoint {
     shell = new Shell( display, SWT.NO_TRIM );
     shell.setLayout( GridLayoutFactory.fillDefaults().margins( 0, 0 ).spacing( 0, 0 ).create() );
     shell.setMaximized( true );
-    shell.setBackground( new Color( display, 0, 0, 0 ) );
-    shell.addControlListener( new ControlAdapter() {
-
-      @Override
-      public void controlResized( ControlEvent e ) {
-        setBrowserUrl( lastLat, lastLon );
-      }
-    } );
   }
 
   private void createTitle() {
@@ -102,34 +81,23 @@ public class GeolocationDemo implements EntryPoint {
     title.setText( "D'oh! Where am I?" );
   }
 
-  private void createBrowser() {
-    browser = new Browser( shell, SWT.NONE );
-    browser.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
-    setBrowserUrl( lastLat, lastLon );
-  }
-
   private Button createGetLocationButton( final Composite container ) {
     Button getLocationButton = new Button( container, SWT.PUSH );
     getLocationButton.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
-    getLocationButton.setText( "Where am I?" );
+    getLocationButton.setText( "Open my position in maps" );
     getLocationButton.setBackground( new Color( getLocationButton.getDisplay(), 60, 60, 60 ) );
     getLocationButton.setForeground( new Color( getLocationButton.getDisplay(), 225, 255, 255 ) );
     final GeolocationListener listener = new GeolocationListener() {
 
+      @Override
       public void positionReceived( Position position ) {
-        lastLabel = "green_1";
-        setBrowserUrl( position.getCoords().getLatitude(), position.getCoords().getLongitude() );
-        String message = "You are in: \n" + getCity( position );
-        openDialog( "Geolocation", message );
+        openLocation( position.getCoords().getLatitude(), position.getCoords().getLongitude() );
         geolocation.removeGeolocationListener( this );
       }
 
+      @Override
       public void errorReceived( PositionError error ) {
-        StringBuilder builder = new StringBuilder();
-        builder.append( "An error occured: \n" );
-        builder.append( "Code: " + error.getCode() );
-        builder.append( "Message: " + error.getMessage() );
-        openDialog( "Error", builder.toString() );
+        openErrorDialog( "Error " + error.getCode(), error.getMessage() );
         geolocation.removeGeolocationListener( this );
       }
     };
@@ -145,61 +113,9 @@ public class GeolocationDemo implements EntryPoint {
     return getLocationButton;
   }
 
-  private String getCity( Position position ) {
-    StringBuilder builder = new StringBuilder();
-    builder.append( "https://maps.googleapis.com/maps/api/geocode/json?latlng=" );
-    builder.append( position.getCoords().getLatitude() );
-    builder.append( "," );
-    builder.append( position.getCoords().getLongitude() );
-    builder.append( "&sensor=false" );
-    try {
-      URL url = new URL( builder.toString() );
-      String json = readStream( url.openStream() );
-      return processJson( json );
-    } catch( Exception shouldNotHappen ) {
-      throw new IllegalStateException( shouldNotHappen );
-    }
-  }
-
-  private String processJson( String json ) {
-    JsonObject object = JsonObject.readFrom( json );
-    JsonArray resultArray = object.get( "results" ).asArray();
-    for( int i = 0; i < resultArray.size(); i++ ) {
-      JsonObject result = resultArray.get( i ).asObject();
-      JsonArray addressComponents = result.get( "address_components" ).asArray();
-      for( int j = 0; j < addressComponents.size(); j++ ) {
-        JsonObject component = addressComponents.get( j ).asObject();
-        String type = component.get( "types" ).asArray().get( 0 ).asString();
-        if( type.equals( "locality" ) ) {
-          return component.get( "long_name" ).asString();
-        }
-      }
-    }
-    return "not found";
-  }
-
-  protected String readStream( InputStream stream ) {
-    final char[] buffer = new char[ 0x10000 ];
-    StringBuilder out = new StringBuilder();
-    Reader in;
-    try {
-      in = new InputStreamReader( stream, "UTF-8" );
-      int read;
-      do {
-        read = in.read( buffer, 0, buffer.length );
-        if( read > 0 ) {
-          out.append( buffer, 0, read );
-        }
-      } while( read >= 0 );
-      return out.toString();
-    } catch( Exception shouldNotHappen ) {
-      throw new IllegalStateException( shouldNotHappen );
-    }
-  }
-
   private Button createSpringfieldButton( final Composite container ) {
     Button button = new Button( container, SWT.PUSH );
-    button.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    button.setLayoutData( GridDataFactory.fillDefaults().align( SWT.FILL, SWT.CENTER ).grab( true, false ).create() );
     button.setText( "Head me to Springfield" );
     button.setBackground( new Color( button.getDisplay(), 225, 151, 7 ) );
     button.setForeground( new Color( button.getDisplay(), 225, 255, 255 ) );
@@ -207,15 +123,8 @@ public class GeolocationDemo implements EntryPoint {
 
       @Override
       public void positionReceived( Position position ) {
-        lastLabel = "yellow_1";
-        setBrowserUrl( SPRINGFIELD_LAT, SPRINGFIELD_LON );
-        double distance = distFrom( position.getCoords().getLatitude(), position.getCoords()
-                                    .getLongitude(), SPRINGFIELD_LAT, SPRINGFIELD_LON );
-        DecimalFormat format = new DecimalFormat( "#0.00" );
-        openDialog( "Springfield Locator", "Distance to Moe's: \n"
-            + format.format( distance )
-            + " km" );
         geolocation.removeGeolocationListener( this );
+        openLocation( SPRINGFIELD_LAT, SPRINGFIELD_LON );
       }
 
     };
@@ -230,75 +139,19 @@ public class GeolocationDemo implements EntryPoint {
     return button;
   }
 
-  private double distFrom( double lat1, double lon1, double lat2, double lon2 ) {
-    double d2r = Math.PI / 180;
-    double dlong = ( lon2 - lon1 ) * d2r;
-    double dlat = ( lat2 - lat1 ) * d2r;
-    double a = Math.pow( Math.sin( dlat / 2.0 ), 2 )
-               + Math.cos( lat1 * d2r )
-               * Math.cos( lat2 * d2r )
-               * Math.pow( Math.sin( dlong / 2.0 ), 2 );
-    double c = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) );
-    return 6367 * c;
-  }
-
-  private void openDialog( String title, String message ) {
-    final Shell box = new Shell( shell, SWT.APPLICATION_MODAL | SWT.BORDER | SWT.TITLE );
-    box.setText( title );
-    GridLayout layout = new GridLayout();
-    layout.marginWidth = 50;
-    box.setLayout( layout );
-    box.setBackground( new Color( box.getDisplay(), 0, 0, 0 ) );
-    Label label = new Label( box, SWT.NONE );
-    label.setText( message );
-    label.setLayoutData( GridDataFactory.fillDefaults()
-      .align( SWT.CENTER, SWT.CENTER )
-      .grab( true, true )
-      .create() );
-    Button close = new Button( box, SWT.PUSH );
-    close.setBackground( new Color( box.getDisplay(), 225, 151, 7 ) );
-    close.setText( "Okily dokily!" );
-    close.setLayoutData( GridDataFactory.fillDefaults()
-      .align( SWT.FILL, SWT.CENTER )
-      .grab( true, false )
-      .create() );
-    close.addSelectionListener( new SelectionAdapter() {
-
-      @Override
-      public void widgetSelected( SelectionEvent e ) {
-        box.close();
-      }
-    } );
-    box.pack();
-    box.addControlListener( new ControlAdapter() {
-
-      @Override
-      public void controlResized( ControlEvent e ) {
-        centerDialog( box );
-      }
-    } );
-    centerDialog( box );
-    box.open();
-  }
-
-  private void centerDialog( Shell box ) {
-    int newX = ( shell.getSize().x - box.getSize().x ) / 2;
-    int newY = ( shell.getSize().y - box.getSize().y ) / 2;
-    box.setLocation( newX, newY );
-  }
-
-  private void setBrowserUrl( double lat, double lon ) {
-    StringBuilder builder = new StringBuilder();
-    builder.append( "http://maps.googleapis.com/maps/api/staticmap" );
-    builder.append( "?size=" + shell.getSize().x + "x" + shell.getSize().y );
-    builder.append( "&zoom=16" );
-    lastLat = lat;
-    lastLon = lon;
-    builder.append( "&center=" + lat + "," + lon );
-    builder.append( "&sensor=false" );
-    if( lastLabel != null ) {
-      builder.append( "&markers=color:yellow%7Clabel:Location%7C" + lat + "," + lon );
+  protected void openLocation( double latitude, double longitude ) {
+    AppLauncher appLauncher = RWT.getClient().getService( AppLauncher.class );
+    if( appLauncher != null ) {
+      MapsOptions mapsOptions = new MapsOptions( latitude, longitude );
+      appLauncher.open( mapsOptions );
     }
-    browser.setUrl( builder.toString() );
   }
+
+  private void openErrorDialog( String title, String message ) {
+    MessageBox box = new MessageBox( shell, SWT.ICON_ERROR );
+    box.setText( title );
+    box.setMessage( message );
+    DialogUtil.open( box, null );
+  }
+
 }
