5e072f16062b8594c1adb386426ae087d1045a14
Ivan Furnadjiev <ivan@eclipsesource.com>
2018-03-14 11:06:13 +0200

    Restore printing functionality in Camera demo

--- com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/CameraDemo.java
+++ com.eclipsesource.tabris.demos/src/com/eclipsesource/tabris/demos/entrypoints/CameraDemo.java
@@ -18,6 +18,8 @@ import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.application.EntryPoint;
 import org.eclipse.rap.rwt.service.ResourceManager;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
@@ -39,11 +41,15 @@ import com.eclipsesource.tabris.camera.CameraOptions;
 import com.eclipsesource.tabris.camera.PhotoAlbum;
 import com.eclipsesource.tabris.camera.PhotoAlbumListener;
 import com.eclipsesource.tabris.camera.PhotoAlbumOptions;
+import com.eclipsesource.tabris.print.PrintError;
+import com.eclipsesource.tabris.print.PrintListener;
+import com.eclipsesource.tabris.print.Printer;
 import com.eclipsesource.tabris.widgets.enhancement.Widgets;
 
 public class CameraDemo implements EntryPoint {
 
   private Label imageLabel;
+  private ToolItem printAction;
 
   @Override
   public int createUI() {
@@ -63,7 +69,6 @@ public class CameraDemo implements EntryPoint {
   private void initCamera() {
     Camera camera = RWT.getClient().getService( Camera.class );
     camera.addCameraListener( new CameraListener() {
-
       @Override
       public void receivedPicture( Image image ) {
         handleImage( image );
@@ -74,7 +79,6 @@ public class CameraDemo implements EntryPoint {
   private void initAlbum() {
     PhotoAlbum album = RWT.getClient().getService( PhotoAlbum.class );
     album.addPhotoAlbumListener( new PhotoAlbumListener() {
-
       @Override
       public void receivedImage( Image image ) {
         handleImage( image );
@@ -84,8 +88,10 @@ public class CameraDemo implements EntryPoint {
   private void handleImage( Image image ) {
     if( image == null ) {
       imageLabel.setText( "Could not provide image from camera" );
+      printAction.setEnabled( false );
     } else {
       imageLabel.setImage( image );
+      printAction.setEnabled( true );
     }
   }
 
@@ -108,6 +114,38 @@ public class CameraDemo implements EntryPoint {
     ToolItem toolItem = new ToolItem( toolBar, SWT.NONE );
     toolItem.setText( "Camera Demo" );
     Widgets.onToolItem( toolItem ).useAsTitle();
+    createPrintToolItem( toolBar );
+  }
+
+  private void createPrintToolItem( ToolBar toolBar ) {
+    printAction = new ToolItem( toolBar, SWT.NONE );
+    printAction.setText( "Print" );
+    printAction.setEnabled( false );
+    printAction.addSelectionListener( new SelectionAdapter() {
+      @Override
+      public void widgetSelected( SelectionEvent e ) {
+        Printer printer = RWT.getClient().getService( Printer.class );
+        if( printer != null ) {
+          printer.addPrintListener( new PrintListener() {
+            @Override
+            public void printSucceeded( String jobName ) {
+              System.out.println( "Print job \"" + jobName + "\" finished succesfuly" );
+            }
+            @Override
+            public void printFailed( PrintError error ) {
+              System.out.println( "Print job \"" + error.getJobName() + "\" failed with error: " + error.getMessage() );
+            }
+            @Override
+            public void printCanceled( String jobName ) {
+              System.out.println( "Print job \"" + jobName + "\" canceled" );
+            }
+          } );
+          Image image = imageLabel.getImage();
+          String url = swtImageAsPNGResourceURL( image );
+          printer.print( url, "Camera print job" );
+        }
+      }
+    } );
   }
 
   private Composite createMainComp( final Shell shell ) {
@@ -138,7 +176,6 @@ public class CameraDemo implements EntryPoint {
       .grab( true, false )
       .create() );
     cameraButton.addListener( SWT.Selection, new Listener() {
-
       @Override
       public void handleEvent( Event event ) {
         Camera photoCamera = RWT.getClient().getService( Camera.class );
@@ -180,11 +217,13 @@ public class CameraDemo implements EntryPoint {
 
   String swtImageAsPNGResourceURL( Image image ) {
     ImageLoader loader = new ImageLoader();
-    loader.data = new ImageData[] {image.getImageData()};
+    loader.data = new ImageData[]{
+      image.getImageData()
+    };
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    loader.save(outputStream, SWT.IMAGE_PNG);
+    loader.save( outputStream, SWT.IMAGE_JPEG );
     ResourceManager manager = RWT.getResourceManager();
-    String name = image.hashCode() + ".png";
+    String name = image.hashCode() + ".jpg";
     manager.register( name, new ByteArrayInputStream( outputStream.toByteArray() ) );
     return manager.getLocation( name );
   }
